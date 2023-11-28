77ce3baa2318ce7ed2621fcb1e72d82eab5c2bbb
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-11-28 01:32:26 -0500

    add calculator

--- pages/calculator.js
+++ pages/calculator.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import Head from 'next/head'
 
 import Script from 'dangerous-html/react'
@@ -6,7 +6,42 @@ import Script from 'dangerous-html/react'
 import Header from '../components/header'
 import Footer from '../components/footer'
 
+const ONE_BILLION = 1000000000;
+
+const AVG_FED_TAX_REMAIN = .60;
+
+const AVG_ACT_60_REMAIN = .96;
+
 const Calculator = (props) => {
+  const [currentIncome, setCurrentIncome] = useState(0);
+
+  const onChangeIncome = (event) => {
+    setCurrentIncome(event.target.value);
+  }
+
+  const formattedIncome = useMemo(() => {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(currentIncome);
+  }, [currentIncome]);
+
+  const netIncomeAverageFederalTaxes = useMemo(() => {
+    const net = currentIncome <= 0 ? 0 : Number(currentIncome * AVG_FED_TAX_REMAIN);
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(net);
+  }, [currentIncome]);
+
+  const netIncomeAverageAct60 = useMemo(() => {
+    const net = currentIncome <= 0 ? 0 : Number(currentIncome * AVG_ACT_60_REMAIN);
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(net);
+  }, [currentIncome]);
+
   return (
     <>
       <div className="calculator-container">
@@ -18,30 +53,22 @@ const Calculator = (props) => {
           />
         </Head>
         <Header rootClassName="header-root-class-name3"></Header>
-        <div className="calculator-container1">
-          <span className="calculator-text">
-            <span>Household Income</span>
+        <div>
+          <div style={{ fontSize: 30 }}>
+            <b>Gross Income</b>
+            <br />
+            <span>{formattedIncome}</span>
             <br></br>
+            <b>Net Income <i>after deducting average taxes</i></b>
+            <br />
+            <span>{netIncomeAverageFederalTaxes}</span>
             <br></br>
-          </span>
-          <div className="calculator-container2">
-            <span className="calculator-text04">
-              <span>0</span>
-              <br></br>
-            </span>
-            <span className="calculator-text07">
-              <span>$100,000,00</span>
-              <br></br>
-            </span>
+            <b>Net Income <i>under act 60</i></b>
+            <br />
+            <span>{netIncomeAverageAct60}</span>
+            <br></br>
           </div>
-          <div>
-            <div className="calculator-container4">
-              <Script
-                html={`  <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
-`}
-              ></Script>
-            </div>
-          </div>
+          <input type="range" style={{ width: '100%' }} min={0} max={ONE_BILLION} value={currentIncome} onChange={onChangeIncome} class="slider" />
         </div>
         <Footer rootClassName="footer-root-class-name1"></Footer>
       </div>
